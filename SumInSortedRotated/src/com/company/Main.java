1a5c785aff78d93308865cbe3d8c8200d3724a93
arunvyas168 <arunvyas27@gmail.com>
2016-04-29 11:20:52 -0700

    Sum In Sorted rotated with modulo and without modulo

--- SumInSortedRotated/src/com/company/Main.java
+++ SumInSortedRotated/src/com/company/Main.java
@@ -3,8 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int[] arr = {6,7,1,2,3,4};
-        int sum  = 5;
+        //int[] arr = {6,7,1,2,3,4};
+        int[] arr = {1,2,3,4,6,7};
+        int sum  = 10;
         if (isSumPresentInSortedRotated(arr,sum)){
             System.out.println("Yes, The sum is present");
         } else {
@@ -12,19 +13,70 @@ public class Main {
         }
     }
 
+    /* Perfect solution:
     public static boolean isSumPresentInSortedRotated(int[] arr, int sum){
         //Find Min and max
         int i;
+        System.out.println(arr.length-1);
         for(i = 0; i <(arr.length-1); i++ ){
+            System.out.println(i);
             if(arr[i] > arr[i+1]){
                 break;
             }
         }
         int endIndex = i;
         int startIndex = (i+1)%(arr.length-1);
-        System.out.println(endIndex);
-        System.out.println(arr.length-1);
-        System.out.println(startIndex);
-        return true;
+
+        while (endIndex!=startIndex){
+            if ((arr[endIndex]+arr[startIndex]) < sum){
+                startIndex = (startIndex+1)%arr.length;
+            } else if((arr[endIndex]+arr[startIndex]) > sum){
+                endIndex = (arr.length+endIndex-1)%arr.length;
+            }else {
+                return true;
+            }
+        }
+        return false;
+    }*/
+
+    /* My solution with lot of conditions: */
+    public static boolean isSumPresentInSortedRotated(int[] arr, int sum) {
+        //Find Min and max
+        int i;
+        int startIndex;
+        int endIndex;
+
+        for (i = 0; i < (arr.length-1); i++) {
+            if (arr[i] > arr[i + 1]) {
+                break;
+            }
+        }
+        if(i == (arr.length-1)){
+            startIndex = 0;
+            endIndex = arr.length-1;
+        } else {
+            startIndex = i+1;
+            endIndex = i;
+        }
+
+        while (endIndex != startIndex){
+            if ((arr[startIndex]+arr[endIndex]) == sum){
+                return true;
+            } else if((arr[startIndex]+arr[endIndex]) < sum){
+                if (startIndex == arr.length-1){
+                    startIndex = 0;
+                } else {
+                    startIndex++;
+                }
+            } else {
+                if (endIndex == 0){
+                    endIndex = arr.length-1;
+                } else {
+                    endIndex--;
+                }
+            }
+        }
+        return false;
     }
+
 }
