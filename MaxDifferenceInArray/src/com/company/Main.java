e5a246662733c8acb1b524be260d0870a7529821
arunvyas168 <arunvyas27@gmail.com>
2016-06-18 15:21:56 -0700

    find Maximum difference between two elements such that

--- MaxDifferenceInArray/src/com/company/Main.java
+++ MaxDifferenceInArray/src/com/company/Main.java
@@ -8,26 +8,6 @@ public class Main {
         System.out.println(maxDiff(array1));
         System.out.println(maxDiff(array2));
     }
-
-    /*
-    This Method will find difference between smallest and the largest element in the array.
-     */
-    /*public static int maxDiff(int[] array){
-        int min = array[0];
-        int max = array[0];
-        for(int i=0;i<array.length;i++){
-            if(array[i]>max){
-                max = array[i];
-            }
-            if(array[i]<min){
-                min = array[i];
-            }
-        }
-        return max-min;
-    }*/
-
-
-
     /*
             This method will find Maximum difference between two elements such that
             larger element appears after the smaller number
@@ -46,4 +26,24 @@ public class Main {
         }
         return maxDifference;
     }
+
+
+     /*
+    This Method will find difference between smallest and the largest element in the array.
+     */
+    /*public static int maxDiff(int[] array){
+        int min = array[0];
+        int max = array[0];
+        for(int i=0;i<array.length;i++){
+            if(array[i]>max){
+                max = array[i];
+            }
+            if(array[i]<min){
+                min = array[i];
+            }
+        }
+        return max-min;
+    }*/
+
+
 }
