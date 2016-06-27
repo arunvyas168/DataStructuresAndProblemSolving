0f8194e5fa46c2684b0e6f46dfab2df9f7eaf5b7
arunvyas168 <arunvyas27@gmail.com>
2016-06-26 18:13:58 -0700

    Spiral_anticlockwise

--- SpiralMatrixPrint/src/com/company/Main.java
+++ SpiralMatrixPrint/src/com/company/Main.java
@@ -6,13 +6,18 @@ public class Main {
         int[][] matrix = {{1, 2, 3, 4, 5},
                           {6, 7, 8, 9, 10},
                           {11,12,13,14,15}};
-        //Expected op: { 1 2 3 4 5 10 15 14 13 12 11 6 7 8 9 }
+        //Clockwise op:         { 1 2 3 4 5 10 15 14 13 12 11 6 7 8 9 }
+        //Anti-Clockwise op:    { 1 6 11 12 13 14 15 10 5 4 3 2 7 8 9 }
         System.out.print("{ ");
-        printSpiral(3,5,matrix);
+        printSpiral_clockwise(3,5,matrix);
+        System.out.print("}");
+        System.out.println();
+        System.out.print("{ ");
+        printSpiral_AntiClockwise(3,5,matrix);
         System.out.print("}");
     }
 
-    public static void printSpiral(int rowEnd,int columnEnd,int[][] matrix){
+    public static void printSpiral_clockwise(int rowEnd, int columnEnd, int[][] matrix){
         int rowStart = 0;
         int columnStart = 0;
 
@@ -36,6 +41,34 @@ public class Main {
                     System.out.print(matrix[i][columnStart]+" ");
                 }
                 columnStart++;
+            }
+        }
+    }
+
+    public static void printSpiral_AntiClockwise(int rowEnd, int columnEnd, int[][] matrix){
+        int rowStart = 0;
+        int columnStart = 0;
+
+        while ((rowStart<rowEnd)&&(columnStart<columnEnd)){
+            for(int i =rowStart;i<rowEnd;i++){
+                System.out.print(matrix[i][columnStart]+" ");
+            }
+            columnStart++;
+            for (int i=columnStart;i<columnEnd;i++){
+                System.out.print(matrix[rowEnd-1][i]+" ");
+            }
+            rowEnd--;
+            if(columnStart<columnEnd) {
+                for (int i = rowEnd - 1; i >= rowStart; i--) {
+                    System.out.print(matrix[i][columnEnd-1]+" ");
+                }
+                columnEnd--;
+            }
+            if(rowStart<rowEnd) {
+                for (int i = columnEnd - 1; i >= columnStart; i--) {
+                    System.out.print(matrix[rowStart][i]+" ");
+                }
+                rowStart++;
             }
         }
     }
