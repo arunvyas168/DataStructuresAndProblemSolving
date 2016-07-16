7d5b0037ad07d2e89cb239f648c69be3022cab76
arunvyas168 <arunvyas27@gmail.com>
2016-07-16 13:12:56 -0700

    Longest prefix matching – A Trie based solution

--- Trie/src/com/company/Main.java
+++ Trie/src/com/company/Main.java
@@ -5,18 +5,40 @@ public class Main {
     public static void main(String[] args) {
 
         Trie trie = new Trie();
-        trie.trieInsert("are");
-        trie.trieInsert("area");
-        trie.trieInsert("base");
-        trie.trieInsert("cat");
-        trie.trieInsert("cater");
-        trie.trieInsert("basement");
+        String[] dictionary =  {"are", "area", "base", "cat", "cater", "children", "basement"};
+        //Construct the TRIE data-structure with the values in the dictionary
+        for (String s : dictionary){
+            trie.trieInsert(s);
+        }
+        //Test Search
         System.out.println(trie.trieSearch("cat"));
+        //Test Delete and Search
         trie.trieDelete("cat");
         System.out.println(trie.trieSearch("cat"));
         System.out.println(trie.trieSearch("cater"));
         trie.trieInsert("cat");
         System.out.println(trie.trieSearch("cat"));
+
+    /*
+        Longest prefix matching
+        Given a dictionary of words and an input string,
+        find the longest prefix of the string which is also a word in dictionary.
+        Let the dictionary contains the following words:
+        {are, area, base, cat, cater, children, basement}
+
+        Below are some input/output examples:
+        --------------------------------------
+        Input String            Output
+        --------------------------------------
+        caterer                 cater
+        basemexy                base
+        child                   < Empty >
+    */
+
+        String[] input = {"caterer", "basemexy", "child"};
+        for (String s : input){
+            System.out.println("Longest Prefix for "+s+" is ====> <"+trie.longestPrefixMatching(s)+">");
+        }
 
     }
 }
--- Trie/src/com/company/Trie.java
+++ Trie/src/com/company/Trie.java
@@ -9,6 +9,9 @@ public class Trie {
         root = new TrieNode();
     }
 
+    /**
+     * Iterative implementation of insert into trie
+     */
     public void trieInsert(String word){
         TrieNode current = this.root;
 
@@ -24,6 +27,9 @@ public class Trie {
         current.isLeaf = true;
     }
 
+    /**
+     * Iterative implementation of search into trie.
+     */
     public boolean trieSearch(String word){
         TrieNode current = this.root;
 
@@ -39,9 +45,17 @@ public class Trie {
         return current.isLeaf;
     }
 
+
+    /**
+     * Delete word from trie.
+     */
     public boolean trieDelete(String word){
         return trieDelete_util(this.root,word,0);
     }
+
+    /**
+     * Returns true if parent should delete the mapping
+     */
     private boolean trieDelete_util(TrieNode root, String word, int index){
         if (index == word.length()){
             if (!root.isLeaf){
@@ -65,5 +79,29 @@ public class Trie {
 
         return false;
 
+    }
+
+    /**
+     * Longest prefix matching for a given word.
+     */
+    public String longestPrefixMatching(String word){
+        TrieNode current = this.root;
+        String output = "";
+        String currentWord = "";
+
+        for (int i=0;i<word.length();i++){
+            char c = word.charAt(i);
+            TrieNode node = current.children.get(c);
+            if (node == null){
+                return output;
+            }else {
+                currentWord += c;
+                if (node.isLeaf){             //remember the match should be an actual word in the dictionary
+                    output=currentWord;
+                }
+                current = node;
+            }
+        }
+        return output;
     }
 }
