898515310b501a6bab5cad803e689f73eadb4409
arunvyas168 <arunvyas27@gmail.com>
2016-10-20 22:51:45 -0700

    minor fix for NextHighestDigitGivenSet

--- NextHighestDigitGivenSet/src/com/company/Main.java
+++ NextHighestDigitGivenSet/src/com/company/Main.java
@@ -6,21 +6,22 @@ public class Main {
         String number = "38276";
         System.out.println(number);
         System.out.println(findNextHighest(number));
+        String number2 = "98765";
+        System.out.println(number2);
+        System.out.println(findNextHighest(number2));
     }
 
     public static String findNextHighest(String num){
         char[] array = num.toCharArray();
-        for(int i=num.length()-1;i>=0;i--){
-            if(i!=0){
-                if((Character.compare(num.charAt(i),num.charAt(i-1)))>0){
-                    char temp = array[i];
-                    array[i]= array[i-1];
-                    array[i-1]=temp;
-                    break;
-                }
+        for(int i=num.length()-1;i>0;i--){
+            if((Character.compare(num.charAt(i),num.charAt(i-1)))>0){
+                char temp = array[i];
+                array[i]= array[i-1];
+                array[i-1]=temp;
+                return String.valueOf(array);
             }
         }
-        return String.valueOf(array);
+        return "Not Possible";
     }
 
 }
