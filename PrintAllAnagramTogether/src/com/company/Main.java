2c78d87d06dfbae7a1c5c85beda81b55c0f9b9e1
arunvyas168 <arunvyas27@gmail.com>
2016-10-21 00:13:39 -0700

    in O(nm) complexity

--- PrintAllAnagramTogether/src/com/company/Main.java
+++ PrintAllAnagramTogether/src/com/company/Main.java
@@ -7,8 +7,58 @@ public class Main {
     public static void main(String[] args) {
         String[] input =  {"cat", "dog", "tac", "god", "act"};
         printAnagramsTogether(input);
+        printAnagramsTogether_leetcode(input);
     }
 
+    /**
+     * The complexity is th O(nm) where m is the average length of strings
+     * and n is the number of string
+     * Building the sb is of constant time
+     * @param input
+     */
+    public static void printAnagramsTogether_leetcode(String[] input){
+        HashMap<String,ArrayList<String>> result  = new HashMap<>();
+        ArrayList<String> list = null;
+        for (String str : input){
+            String anagram = anagramBuilder(str);
+            if (!result.containsKey(anagram)){
+                list = new ArrayList<>();
+                list.add(str);
+                result.put(anagram,list);
+            }else {
+                ArrayList<String> temp = result.get(anagram);
+                temp.add(str);
+                result.put(anagram,temp);
+            }
+        }
+
+        for(Map.Entry<String, ArrayList<String>> entry : result.entrySet()){
+            ArrayList<String> group = entry.getValue();
+            for (String s : group){
+                System.out.print(s+" ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static String anagramBuilder(String str){
+        int[] anagram = new int[256];
+        for (int i=0;i<str.length();i++){
+            anagram[str.charAt(i)]++;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i : anagram){
+            sb.append(i);
+        }
+        return sb.toString();
+    }
+
+
+    /**
+     * The complexity is th O(nmLogm) where m is the average length of strings
+     * and n is the number of string
+     * @param input
+     */
     public static void printAnagramsTogether(String[] input){
         Text t1;
         Comparator<Text> comp = new Text();
